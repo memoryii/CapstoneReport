77c4e7095e957188d1cd0d41bfa9d893e255ec52
memoryii <skdmlqnsrlt@gmail.com>
2025-05-18 00:59:18 +0900

    src folder update

---
+++ src/App_1.jsx
@@ -0,0 +1,736 @@
+import React from "react";
+
+import "./style.css";
+import image2 from "./images/image2.png";
+import image10 from "./images/image10.png";
+import image11 from "./images/image11.png";
+import image12 from "./images/image12.png";
+import image from "./images/image.svg";
+import polygon2 from "./polygons/Polygon2.svg";
+import polygon3 from "./polygons/Polygon3.svg";
+import polygon4 from "./polygons/Polygon4.svg";
+import polygon5 from "./polygons/Polygon5.svg";
+
+import vector1 from "./vectors/Vector1.svg";
+import vector3 from "./vectors/Vector3.svg";
+import vector4 from "./vectors/Vector4.svg";
+import vector5 from "./vectors/Vector5.svg";
+import vector6 from "./vectors/Vector6.svg";
+import vector7 from "./vectors/Vector7.svg";
+import vector8 from "./vectors/Vector8.svg";
+import vector9 from "./vectors/Vector9.svg";
+import vector11 from "./vectors/Vector11.svg";
+import vector12 from "./vectors/Vector12.svg";
+import vector13 from "./vectors/Vector13.svg";
+import vector14 from "./vectors/Vector14.svg";
+import vector15 from "./vectors/Vector15.svg";
+import vector16 from "./vectors/Vector16.svg";
+import vector17 from "./vectors/Vector17.svg";
+
+export const Iphone = () => {
+  return (
+    <div className="iphone">
+      <div className="div">
+        <div className="overlap">
+          <div className="auto-layout">
+            <div className="text-wrapper">
+              꽃을 통한 내면의 시각화
+            </div>
+          </div>
+
+          <div className="div-wrapper">
+            <p className="p">
+              다양한 형태와 색을 지닌
+              꽃으로 관람자의 내면
+              감정과 자아 이미지를
+              은유적으로 표현합니다.
+            </p>
+          </div>
+
+          <div className="text-wrapper-2">
+            03
+          </div>
+        </div>
+
+        <div className="overlap-group">
+          <div className="auto-layout-2">
+            <div className="text-wrapper">
+              욕망이라는 감정의 탐색
+            </div>
+          </div>
+
+          <div className="auto-layout-3">
+            <p className="text-wrapper-3">
+              작품은 자기애, 욕망,
+              진실된 내면을 중심으로
+              구성되며, 인간 내면의
+              복잡한 감정과 마주하게
+              합니다.
+            </p>
+          </div>
+
+          <div className="text-wrapper-4">
+            02
+          </div>
+        </div>
+
+        <div className="overlap-group-2">
+          <div className="overlap-2">
+            <div className="helleborus">
+              {" "}
+              Helleborus
+            </div>
+
+            <div className="text-wrapper-5">
+              white
+            </div>
+          </div>
+
+          <div className="text-wrapper-6">
+            DEEP SYNC
+          </div>
+
+          <div className="text-wrapper-7">
+            INFLORIA
+          </div>
+
+          <div className="auto-layout-4">
+            <div className="div-2">
+              Empathic Engagement
+            </div>
+          </div>
+
+          <p className="text-wrapper-8">
+            Your Unconsious is “Deep
+            Sync”
+          </p>
+
+          <p className="div-3">
+            <span className="span">
+              짧은 순간, 당신의 시선이
+              보여준 것은{" "}
+            </span>
+
+            <span className="text-wrapper-9">
+              깊은 감정적 반응과, 내면의
+              집중 그리고 지속되는 응시
+            </span>
+
+            <span className="span">
+              였습니다. 그에 따라,
+              당신의 내면에서 피어난
+              진실된 꽃은,
+            </span>
+
+            <span className="text-wrapper-10">
+              &nbsp;
+            </span>
+
+            <span className="text-wrapper-9">
+              온화한 하얀빛의 고고한
+              헬레보어
+            </span>
+
+            <span className="span">
+              입니다.
+            </span>
+          </p>
+
+          <div className="overlap-3">
+            <div className="text-wrapper-11">
+              Result Report
+            </div>
+          </div>
+
+          <img
+            className="vector"
+            alt="Vector"
+            src={vector11}
+          />
+        </div>
+
+        <div className="overlap-4">
+          <div className="overlap-5">
+            <img
+              className="image"
+              alt="Image"
+              src={image12}
+            />
+
+            <img
+              className="img"
+              alt="Image"
+              src={image11}
+            />
+
+            <img
+              className="image-2"
+              alt="Image"
+              src={image10}
+            />
+
+            <img
+              className="image-3"
+              alt="Image"
+              src={image2}
+            />
+
+            <div className="ellipse" />
+
+            <div className="ellipse-2" />
+
+            <div className="ellipse-3" />
+
+            <div className="ellipse-4" />
+
+            <div className="ellipse-5" />
+
+            <div className="ellipse-6" />
+
+            <div className="ellipse-7" />
+
+            <div className="ellipse-8" />
+
+            <div className="ellipse-9" />
+
+            <div className="ellipse-10" />
+
+            <div className="ellipse-11" />
+
+            <div className="ellipse-12" />
+
+            <div className="ellipse-13" />
+
+            <div className="ellipse-14" />
+
+            <div className="ellipse-15" />
+
+            <div className="ellipse-16" />
+
+            <div className="ellipse-17" />
+
+            <div className="ellipse-18" />
+
+            <div className="ellipse-19" />
+
+            <div className="text-wrapper-12">
+              COLOR SELECTION
+            </div>
+
+            <div className="auto-layout-5">
+              <p className="div-2">
+                <span className="text-wrapper-13">
+                  당신의 감정을 나타내는
+                  색상은
+                </span>
+
+                <span className="text-wrapper-14">
+                  {" "}
+                  불멸, 순수, 자존과
+                  순결을 상징하는 “흰색“{" "}
+                </span>
+
+                <span className="text-wrapper-13">
+                  입니다
+                </span>
+              </p>
+            </div>
+
+            <p className="text-wrapper-15">
+              시선이 화면 전체에
+              <br />
+              얼마나 넓게 분포
+            </p>
+
+            <div className="text-wrapper-16">
+              ZONE DIVERSITY
+            </div>
+
+            <p className="text-wrapper-17">
+              화면을 여러 구역으로
+              <br />
+              나누었을 때 얼마나 다양한
+              영역을 탐색했는지
+            </p>
+
+            <div className="text-wrapper-18">
+              TRANSITION FREQUENCY
+            </div>
+
+            <p className="text-wrapper-19">
+              시선이 한 지점에서
+              <br />
+              다른 지점으로 얼마나{" "}
+              <br />
+              자주 이동하는지
+            </p>
+
+            <div className="text-wrapper-20">
+              FIXATION COUNT
+            </div>
+
+            <p className="text-wrapper-21">
+              전체 분석 구간에서 발생한{" "}
+              <br />
+              시선 고정의 총 개수
+            </p>
+
+            <img
+              className="vector-2"
+              alt="Vector"
+              src={vector15}
+            />
+
+            <img
+              className="vector-3"
+              alt="Vector"
+              src={vector1}
+            />
+
+            <img
+              className="vector-4"
+              alt="Vector"
+              src={image}
+            />
+
+            <img
+              className="vector-5"
+              alt="Vector"
+              src={vector3}
+            />
+
+            <img
+              className="vector-6"
+              alt="Vector"
+              src={vector4}
+            />
+
+            <img
+              className="vector-7"
+              alt="Vector"
+              src={vector6}
+            />
+
+            <img
+              className="vector-8"
+              alt="Vector"
+              src={vector5}
+            />
+
+            <img
+              className="vector-9"
+              alt="Vector"
+              src={vector7}
+            />
+          </div>
+
+          <div className="text-wrapper-22">
+            DATA ITEM
+          </div>
+
+          <div className="auto-layout-6">
+            <p className="div-2">
+              무의식 유형 분석을 위한
+              시선트래킹 데이터 항목
+            </p>
+          </div>
+
+          <div className="auto-layout-7">
+            <p className="text-wrapper-23">
+              영상을 진행하며, 당신의
+              무의식속 내면을 반영하는
+              지표인 시선을
+              분석했습니다. 저희가
+              분석한 시선 데이터의
+              항목은 다음과 같습니다.
+            </p>
+          </div>
+
+          <div className="text-wrapper-24">
+            FIXATION TIME
+          </div>
+
+          <div className="text-wrapper-25">
+            한 지점에 머무르는
+            <br />
+            평균 시간
+          </div>
+
+          <div className="text-wrapper-26">
+            DISPERSION
+          </div>
+
+          <img
+            className="vector-10"
+            alt="Vector"
+            src={vector13}
+          />
+        </div>
+
+        <div className="overlap-6">
+          <div className="text-wrapper-27">
+            INFLORIA
+          </div>
+
+          <div className="overlap-7">
+            <div className="text-wrapper-28">
+              Made by Team
+            </div>
+          </div>
+
+          <p className="KIM-YUJIN-KIM">
+            KIM YUJIN
+            <br />
+            KIM DONGHEE
+            <br />
+            KIM SARA
+            <br />
+            PARK LEEKYUNG
+            <br />
+            PARK KIGEONG
+          </p>
+        </div>
+
+        <div className="overlap-8">
+          <div className="ellipse-20" />
+
+          <div className="ellipse-21" />
+
+          <div className="ellipse-22" />
+
+          <div className="ellipse-23" />
+
+          <div className="ellipse-24" />
+
+          <div className="ellipse-25" />
+
+          <div className="ellipse-26" />
+
+          <div className="ellipse-27" />
+
+          <div className="ellipse-28" />
+
+          <div className="ellipse-29" />
+
+          <div className="ellipse-30" />
+
+          <div className="ellipse-31" />
+
+          <div className="ellipse-32" />
+
+          <div className="text-wrapper-29">
+            INNER KEYWORD
+          </div>
+
+          <div className="auto-layout-8">
+            <p className="div-4">
+              <span className="text-wrapper-13">
+                당신은 지금 자신을
+                드러내고 싶은{" "}
+              </span>
+
+              <span className="text-wrapper-14">
+                강한 표현 욕구와
+                사랑받고자 하는 마음이
+                피어오른 상태
+              </span>
+
+              <span className="text-wrapper-13">
+                입니다.
+              </span>
+            </p>
+          </div>
+
+          <div className="auto-layout-9">
+            <p className="div-4">
+              활짝 피어난 꽃 - 자신감,
+              애정 결핍, 사회적 표현
+            </p>
+          </div>
+        </div>
+
+        <div className="text-wrapper-30">
+          Internal data classification
+          type
+        </div>
+
+        <div className="auto-layout-10">
+          <p className="text-wrapper-31">
+            당신의 내면의 꽃을 피어낸
+            데이터 분석 값
+          </p>
+        </div>
+
+        <div className="text-wrapper-32">
+          Exploring the Unconscious Self
+        </div>
+
+        <div className="text-wrapper-33">
+          Exploring the Unconscious Self
+        </div>
+
+        <div className="text-wrapper-34">
+          FIXATION TIME
+        </div>
+
+        <div className="text-wrapper-35">
+          고정 시간
+        </div>
+
+        <div className="text-wrapper-36">
+          ZONE DIVERSITY
+        </div>
+
+        <div className="text-wrapper-37">
+          관찰 영역 다양성
+        </div>
+
+        <div className="text-wrapper-38">
+          DISPERSION
+        </div>
+
+        <div className="text-wrapper-39">
+          시선 분산도
+        </div>
+
+        <div className="text-wrapper-40">
+          TRANSITION FREQUENCY
+        </div>
+
+        <div className="text-wrapper-41">
+          전환 빈도
+        </div>
+
+        <div className="text-wrapper-42">
+          FIXATION COUNT
+        </div>
+
+        <div className="text-wrapper-43">
+          고정 횟수
+        </div>
+
+        <div className="overlap-9">
+          <img
+            className="polygon"
+            alt="Polygon"
+            src={polygon2}
+          />
+
+          <img
+            className="polygon-2"
+            alt="Polygon"
+            src={polygon3}
+          />
+
+          <img
+            className="polygon-3"
+            alt="Polygon"
+            src={polygon4}
+          />
+
+          <img
+            className="polygon-4"
+            alt="Polygon"
+            src={polygon5}
+          />
+
+          <img
+            className="vector-11"
+            alt="Vector"
+            src={vector8}
+          />
+        </div>
+
+        <img
+          className="vector-12"
+          alt="Vector"
+          src={vector12}
+        />
+
+        <img
+          className="vector-13"
+          alt="Vector"
+          src={vector16}
+        />
+
+        {/*<img
+          className="vector-14"
+          alt="Vector"
+          src={vector10}
+        />*/}
+
+        <img
+          className="vector-15"
+          alt="Vector"
+          src={vector9}
+        />
+
+        <img
+          className="vector-16"
+          alt="Vector"
+          src={vector17}
+        />
+
+        <div className="overlap-10">
+          <div className="auto-layout-11">
+            <div className="text-wrapper-44">
+              시선 기반 인터랙션
+            </div>
+          </div>
+
+          <div className="auto-layout-12">
+            <p className="text-wrapper-45">
+              관람객의 ‘시선’을 추적하여
+              시각적 요소에 직접 영향을
+              주는 인터랙티브 미디어아트
+              작품입니다.
+            </p>
+          </div>
+
+          <div className="text-wrapper-46">
+            01
+          </div>
+        </div>
+
+        <div className="overlap-11">
+          <div className="auto-layout-13">
+            <div className="text-wrapper-47">
+              몰입적이고 시적인 영상
+              경험
+            </div>
+          </div>
+
+          <div className="auto-layout-14">
+            <p className="p">
+              그리스 신화 나르키소스를
+              모티프로 한 서사적 전개와
+              상징적 이미지가 몰입감을
+              줍니다.
+            </p>
+          </div>
+
+          <div className="text-wrapper-48">
+            04
+          </div>
+        </div>
+
+        <div className="text-wrapper-49">
+          BLOOMING DESIRE
+        </div>
+
+        <img
+          className="vector-17"
+          alt="Vector"
+          src={vector14}
+        />
+
+        <div className="flexcontainer">
+          <p className="text">
+            <span className="text-wrapper-50">
+              {" "}
+              관람객의{" "}
+            </span>
+
+            <span className="text-wrapper-51">
+              시선에 따라 내면의 감정과
+              욕망이 <br />
+            </span>
+          </p>
+
+          <p className="text">
+            <span className="text-wrapper-51">
+              꽃의 형상으로 피어나는
+            </span>
+
+            <span className="text-wrapper-50">
+              {" "}
+              인터랙티브 미디어아트
+            </span>
+          </p>
+        </div>
+
+        <div className="flexcontainer-2">
+          <p className="text">
+            <span className="text-wrapper-50">
+              당신이 무심코 머문 시선
+              하나가, <br />
+            </span>
+          </p>
+
+          <p className="text">
+            <span className="text-wrapper-50">
+              내면의 감정을 깨우고
+              하나의 꽃을 피웁니다.
+              <br />
+            </span>
+          </p>
+
+          <p className="text">
+            <span className="text-wrapper-50">
+              각기 다른 색과 형태로
+              피어나는 꽃은 <br />
+            </span>
+          </p>
+
+          <p className="text">
+            <span className="text-wrapper-50">
+              단순한 아름다움을 넘어,{" "}
+              <br />
+            </span>
+          </p>
+
+          <p className="text">
+            <span className="text-wrapper-51">
+              우리 안에 자리한 자아의
+              진실과 욕망을 상징
+            </span>
+
+            <span className="text-wrapper-50">
+              합니다.
+              <br />
+            </span>
+          </p>
+
+          <p className="text">
+            <span className="text-wrapper-50">
+              {"  "}마치 자신을 바라보다
+              물속으로 빠져든
+              나르키소스처럼,
+              <br />
+            </span>
+          </p>
+
+          <p className="text">
+            <span className="text-wrapper-50">
+              {" "}
+              《Blooming Desire》는
+              당신에게 묻습니다.
+              <br />
+            </span>
+          </p>
+
+          <p className="text">
+            <span className="text-wrapper-50">
+              {" "}
+            </span>
+
+            <span className="text-wrapper-51">
+              {" "}
+              지금, 당신의 시선은 어떤
+              감정을 담은 꽃으로
+              피어났나요?
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// App 컴포넌트를 default export
+const App = () => (
+  <div>
+    <Iphone />
+  </div>
+);
+export default App;
